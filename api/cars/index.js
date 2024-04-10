87e14fa1ddc37e96ccd807fd607d2604f0886454
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com.>
2024-04-10 19:49:19 +0200

    Updated index.js

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,13 +1,25 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    const cars = require('./cars.json');
+    if (req.method == "GET")
+    {
+        context.res.json(cars);
+    }
+    else if (req.method == "DELETE")
+    {
+        const id = req.params.id;
+        const index = cars.findIndex(car => car.id === id);
+        cars.splice(index, 1);
+        context.res.json({ message: `Car with id ${id} deleted` });
+    }
+    else if (req.method == "POST")
+    {
+        console.log(req);
+        const newCar = req.body;
+        const lastCar = cars[cars.length-1];
+        const id = parseInt(lastCar["id"])+1;
+        newCar["id"] = id.toString();
+        console.log(newCar);
+        cars.push(newCar);
+        context.res.json(newCar);
+    }
 }
